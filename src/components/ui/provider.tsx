719c2311ae68f9962afdab66cbd2da46dba99f95
J Stenhouse <james_stenhouse@hotmail.com>
2025-04-23 11:05:48 +0100

    dark mode

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,15 @@
-"use client"
-
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+"use client";
+import { ThemeProvider } from "next-themes";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { theme } from "@/theme";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <ChakraProvider value={theme}>
+        <ColorModeProvider {...props} />
+      </ChakraProvider>
+    </ThemeProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+import { ColorModeProvider } from "@/components/ui/color-mode";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "./App.tsx";
-
+import { theme } from "@/theme.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={theme}>
+      <ColorModeProvider defaultTheme="dark">
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>
 );
